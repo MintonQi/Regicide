c580ea41b77c8bac771bf054d1e12a17cbd1a3b7
Qi Mingtong <qimingtong518@gmail.com>
2022-11-24 10:38:27 +0800

    utility modified

--- utility.c
+++ utility.c
@@ -5,12 +5,12 @@
 
 #include "utility.h"
 
-char *display[] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+char *vnames[] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+char *suits[] = {"Spade", "Club", "Heart", "Diamond"};
 
 void shuffle(void *arr, int size)
 {
 	struct card *cards = (struct card *)arr;
-
 	for (int i = 0; i < size; i++) {
 		int j = rand() % size;
 		// printf("%d\n", j);
@@ -25,25 +25,9 @@ void printCard(struct card *cards, int start, int end)
 	int count = 0;
 	for (int i = start; i <= end; i++) {
 		for (int j = 0; j < 4; j++) {
-			switch (j) {
-			case 0:
-				cards[count].suit = "Spade";
-				break;
-			case 1:
-				cards[count].suit = "Club";
-				break;
-			case 2:
-				cards[count].suit = "Heart";
-				break;
-			case 3:
-				cards[count].suit = "Diamond";
-				break;
-			default:
-				break;
-			}
-
+			cards[count].suit = suits[j];
 			cards[count].value = i;
-			cards[count].vname = display[i - 1];
+			cards[count].vname = vnames[i - 1];
 			count++;
 		}
 	}
